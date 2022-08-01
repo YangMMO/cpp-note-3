c0ff3942ae822da3022f07aea2bd6f9d60732288
YangMMO <11436052@qq.com>
2022-08-01 17:55:17 +0800

    push

---
+++ P4/6_3_继承中的对象模型.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//#include <string>
+//
+///* 继承的基本语法 */
+//
+//class MyPrint {
+//public:
+//	
+//};
+//
+//void test01() {
+//	
+//}
+//
+//
+//int main() {
+//
+//	test01();
+//
+//	system("pause");
+//	return 0;
+//}
